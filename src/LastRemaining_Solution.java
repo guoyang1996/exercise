2e59553b361bb086fdc2cd45ca8eb4183f63d147
郭阳 <1305584135@qq.com>
2018-04-24 09:26:18 +0800

    添加约瑟夫环问题的实现
    Signed-off-by: 郭阳 <1305584135@qq.com>

---
+++ src/LastRemaining_Solution.java
@@ -0,0 +1,36 @@
+public class LastRemaining_Solution {
+    public int LastRemaining_Solution(int n, int m) {
+        if(n<=0){
+            return -1;
+        }
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = 1;//1表示其还未出队列
+        }
+        int num = n;
+        int count = m-1;
+        int index = 0;
+        for (index = 0; ; index++) {
+            if (array[index%n] == 1) {
+                if (count > 0) {
+                    count--;//报数
+                } else {
+                    array[index%n] = 0;//出列
+                   // System.out.print(index%n+" ");
+                    num--;
+                    count = m-1;
+                }
+            }
+           if(num==0){
+              // System.out.println();
+                return index%n;
+
+           }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LastRemaining_Solution().LastRemaining_Solution(5, 3));
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int StrToInt(String str) {
+
+        return 0;
+    }
+}
