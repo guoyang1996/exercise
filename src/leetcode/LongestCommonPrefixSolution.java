0bd7c45854683ded12463eb23ee07124776574ea
郭阳 <1305584135@qq.com>
2019-01-21 15:36:24 +0800

    添加求最长公共前缀的代码
    Signed-off-by: 郭阳 <1305584135@qq.com>

--- src/leetcode/LongestCommonPrefixSolution.java
+++ src/leetcode/LongestCommonPrefixSolution.java
@@ -2,7 +2,31 @@ package leetcode;
 
 public class LongestCommonPrefixSolution {
     public String longestCommonPrefix(String[] strs) {
-        return "";
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        boolean isEnd = false;
+        int i = 0;
+        String prefix = "";
+        while (!isEnd) {
+            if (i >= strs[0].length()) {
+                isEnd = true;
+            }
+            prefix = strs[0].substring(0, i);
+            for (String str : strs) {
+                if (!str.startsWith(prefix)) {
+                    isEnd = true;
+                    prefix=prefix.substring(0,prefix.length()-1);
+                    break;
+                }
+            }
+            i++;
+        }
+        return prefix;
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestCommonPrefixSolution().longestCommonPrefix(new String[]{"a", "b"}));
     }
 }
